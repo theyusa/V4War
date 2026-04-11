bdf1ddd311b6ba23e3c141b99652cfde8b4ac572
TheYusa <theyusa@users.noreply.github.com>
2026-04-11 17:26:16 +0330

    fix(ci): make optional library dependencies conditional in composeApp

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -80,8 +80,12 @@ kotlin {
                 implementation(libs.filekit.dialogs.compose)
                 implementation(libs.aboutlibraries.compose.m3)
                 implementation(libs.zxing.core)
-                implementation(project(":library:compose-code-editor:codeeditor"))
-                implementation(project(":library:DragDropSwipeLazyColumn"))
+                rootProject.findProject(":library:compose-code-editor:codeeditor")?.let {
+                    implementation(it)
+                }
+                rootProject.findProject(":library:DragDropSwipeLazyColumn")?.let {
+                    implementation(it)
+                }
 
                 implementation(project.dependencies.platform(libs.koin.bom))
                 implementation(libs.koin.core)
