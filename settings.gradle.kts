44584e836f4fb78b65fc424fea98a33b5993dce4
theyusa <bilalyusaozer@gmail.com>
2026-04-26 21:55:52 +0000

    remove: plugin build scripts, plugin dirs (juicity, naive, mieru, shadowquic), settings.gradle.kts plugin refs, version properties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,10 +21,6 @@ val buildPlugin = System.getenv("BUILD_PLUGIN")
 when {
     buildPlugin.isNullOrBlank() -> {
         include(":plugin:hysteria2")
-        include(":plugin:juicity")
-        include(":plugin:naive")
-        include(":plugin:mieru")
-        include(":plugin:shadowquic")
     }
     buildPlugin == "none" -> {
     }
