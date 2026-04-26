acd5f9556ac0bd21011b98c0836acaf8a6ba3a28
theyusa <bilalyusaozer@gmail.com>
2026-04-26 21:55:44 +0000

    remove: Go plugin implementations - http, juicity, trusttunnel, mieruproto, naive outbound; clean up pluginoption constants

--- libcore/plugin/pluginoption/constant.go
+++ libcore/plugin/pluginoption/constant.go
@@ -5,18 +5,6 @@ import (
 	C "github.com/sagernet/sing-box/constant"
 )
 
-const (
-	TypeJuicity     = "juicity"
-	TypeTrustTunnel = "trusttunnel"
-)
-
 func ProxyDisplayName(proxyType string) string {
-	switch proxyType {
-	case TypeJuicity:
-		return "Juicity"
-	case TypeTrustTunnel:
-		return "TrustTunnel"
-	default:
-		return C.ProxyDisplayName(proxyType)
-	}
+	return C.ProxyDisplayName(proxyType)
 }
